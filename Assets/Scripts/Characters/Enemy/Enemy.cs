f6c6e1d61e94a818a0a5f42ddc6b648241b9ee2b
MattOrbita <mattorbita246@gmail.com>
2025-10-06 15:44:47 -0700

    Start on enemy waypoints
    I'm falling into an overthinking trap

--- Assets/Scripts/Characters/Enemy/Enemy.cs
+++ Assets/Scripts/Characters/Enemy/Enemy.cs
@@ -1,10 +1,19 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Enemy : MonoBehaviour
 {
+    [Header("General")]
     [SerializeField] float moveSpeed;
+
+    [Header("Waypoints")]
+    [SerializeField] int targetWaypointIndex = 0;
+    [SerializeField] List<Transform> waypoints;
+
+    [Header("States")]
     [SerializeField] bool isActive;
+    [SerializeField] bool isPatrolling;
 
     public void ActivateEnemy(bool setActive)
     {
@@ -15,8 +24,46 @@ public class Enemy : MonoBehaviour
     {
         if (isActive)
         {
-            FollowPlayer();
+            if (isPatrolling)
+            {
+                DoPatrolRoutine();
+            }
+            else
+            {
+                FollowPlayer();
+            }
         }
+    }
+
+    void DoPatrolRoutine()
+    {
+        // can't do a patrol routine if there are no waypoints to begin with
+        if (waypoints.Count == 0)
+        {
+            return;
+        }
+
+        // go towards current target waypoint
+        Transform targetWaypoint = waypoints[targetWaypointIndex];
+        Vector3 moveDirection = targetWaypoint.position - transform.position;
+
+        Vector3 fromVector = Vector3.right;
+        int oldAngle = (int) Vector3.Angle(fromVector, moveDirection);
+
+        moveDirection.Normalize();
+        // Mathf.RoundToInt();
+
+        
+        // float oldAngle = Vector3.Angle(fromVector, moveDirection);
+
+        transform.position += Time.deltaTime * moveSpeed * moveDirection;
+
+        // once the angle of the vector from enemy to waypoint changes, that means the enemy has PASSED
+        // their waypoint, meaning it's time to switch to the next waypoint!
+        float newAngle = Vector3.Angle(fromVector, targetWaypoint.position - transform.position);
+
+        bool isAtWaypoint = Vector3.Distance(transform.position, targetWaypoint.position) < Mathf.Epsilon;
+        // bool hasPassedWaypoint = ;
     }
 
     void FollowPlayer()
